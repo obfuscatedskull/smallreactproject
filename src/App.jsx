404f9efbf8e73c239deee5c53f58253cfed55707
maky <rikiki12203@gmail.com>
2026-01-09 19:48:18 -0500

    added main app

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import CardList from "./CardList.jsx";
+import searchBox from "./searchBox.jsx"
+
+
+const App = () => {
+    return (
+        <>
+            <h1>Boba's Friends</h1>
+            <CardList />
+        </>
+    )
+}
+
+
+export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import CardList from './CardList.jsx';
+import App from './App.jsx';
 import 'tachyons'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <CardList />
+    <App />
   </StrictMode>,
 )
+
+
+
